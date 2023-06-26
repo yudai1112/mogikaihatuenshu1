0fc5bc09de3b50216c9cf1461fac3f83c68ac6d9
yudai1112 <yudaishima96@gmail.com>
2023-06-26 09:35:26 +0900

    aa

---
+++ uniform_admin_teamE3/WEB-INF/src/bean/Cart.java
@@ -0,0 +1,87 @@
+package bean;
+
+public class Cart {
+
+	private String selectedUni;
+
+	private int sSize;
+
+	private int mSize;
+
+	private int lSize;
+
+	private int kidsSize;
+
+	private int total;
+
+	private String bikou;
+
+	public Cart(){
+		this.selectedUni = null;
+		this.sSize = 0;
+		this.mSize = 0;
+		this.lSize = 0;
+		this.kidsSize = 0;
+		this.total = sSize + mSize + lSize + kidsSize;
+		String bikou = null;
+
+	}
+
+	public String getSelectedUni() {
+		return selectedUni;
+	}
+
+	public void setSelectedUni(String selectedUni) {
+		this.selectedUni = selectedUni;
+	}
+
+	public int getsSize() {
+		return sSize;
+	}
+
+	public void setsSize(int sSize) {
+		this.sSize = sSize;
+	}
+
+	public int getmSize() {
+		return mSize;
+	}
+
+	public void setmSize(int mSize) {
+		this.mSize = mSize;
+	}
+
+	public int getlSize() {
+		return lSize;
+	}
+
+	public void setlSize(int lSize) {
+		this.lSize = lSize;
+	}
+
+	public int getKidsSize() {
+		return kidsSize;
+	}
+
+	public void setKidsSize(int kidsSize) {
+		this.kidsSize = kidsSize;
+	}
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
+	public String getBikou() {
+		return bikou;
+	}
+
+	public void setBikou(String bikou) {
+		this.bikou = bikou;
+	}
+
+
+}
--- uniform_admin_teamE3/WEB-INF/src/dao/UniformDAO.java
+++ uniform_admin_teamE3/WEB-INF/src/dao/UniformDAO.java
@@ -6,6 +6,7 @@ package dao;
 
 import java.sql.*;
 import bean.*;
+import java.util.ArrayList;
 
 public class UniformDAO {
 
@@ -41,5 +42,136 @@ public class UniformDAO {
 		} catch (Exception e) {
 			throw new IllegalStateException(e);
 		}
+	}
+
+	public ArrayList<Uniform> selectList() {
+		// DB接続
+		Connection con = null;
+		Statement smt = null;
+
+		// SQL文
+		String sql = "SELECT uniform_name, price, img FROM uniform_info ORDER BY uniform_name";
+
+		// ArrayList
+		ArrayList<Uniform> uniform_list = new ArrayList<Uniform>();
+		try {
+			con = getConnection();
+			smt = con.createStatement();
+
+			//
+			ResultSet rs = smt.executeQuery(sql);
+
+			// 得た情報を格納
+			while (rs.next()) {
+				Uniform uni = new Uniform();
+				uni.setName(rs.getString("uniform_name"));
+				uni.setPrice(rs.getInt("price"));
+				uni.setImage(rs.getString("img"));
+				uniform_list.add(uni);
+			}
+		} catch (Exception e) {
+			throw new IllegalStateException(e);
+		} finally {
+			if (smt != null) {
+				try {
+					smt.close();
+				} catch (SQLException ignore) {
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException ignore) {
+				}
+			}
+		}
+		return uniform_list;
+	}
+
+	// DBのユニフォーム情報を格納するuniform_infoテーブルから全ユニフォーム情報を取得するメソッド
+
+	public ArrayList<Uniform> selectAll() {
+
+		// SQL文
+		ArrayList<Uniform> list = new ArrayList<Uniform>();
+		String sql = "SELECT uniformid,uniformname,size,price,inventory,image FROM uniform_info ORDER BY uniformid";
+
+		// 変数宣言
+		Connection con = null;
+		Statement smt = null;
+
+		try {
+			// DB接続
+			con = UniformDAO.getConnection();
+			smt = con.createStatement();
+
+			// SQLをDBへ発行
+			ResultSet rs = smt.executeQuery(sql);
+
+			while (rs.next()) {
+				Uniform uniform = new Uniform();
+				uniform.setUniformid(rs.getString("uniformid"));
+				uniform.setName(rs.getString("uniformname"));
+				uniform.setSize(rs.getString("size"));
+				uniform.setPrice(rs.getInt("price"));
+				uniform.setInventory(rs.getInt("inventory"));
+				uniform.setImage(rs.getString("image"));
+
+				list.add(uniform);
+			}
+
+		} catch (Exception e) {
+			throw new IllegalStateException(e);
+		} finally {
+			if (smt != null) {
+				try {
+					smt.close();
+				} catch (SQLException ignore) {
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException ignore) {
+				}
+			}
+		}
+		return list;
+	}
+
+
+
+	public void delete(String uniformid) {
+
+		// 変数宣言
+		Connection con = null; // コネクション
+		Statement smt = null; // ステートメント
+
+		// SQL文
+		String sql = "DELETE FROM uniform_db WHERE uniformid ='" + uniformid + "'";
+
+		try {
+			// DBに接続しSQL実行準備
+			con = getConnection();
+			smt = con.createStatement();
+
+			// SQL実行
+			smt.executeUpdate(sql);
+
+		} catch (Exception e) {
+			throw new IllegalStateException(e);
+		} finally {
+			try {
+				// コネクションとステートメントのクローズ
+				if (smt != null) {
+					smt.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+			}
+		}
+
 	}
 }
